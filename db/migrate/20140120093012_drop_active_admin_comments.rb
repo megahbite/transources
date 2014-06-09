9fc4321f9310fed132beaea25570792cdcb496f3
Megan Bowra-Dean <megan@rabidtech.co.nz>
2014-06-09 23:49:39 +1200

    Remove unneeded migrations.

--- db/migrate/20140120093012_drop_active_admin_comments.rb
+++
@@ -1,9 +0,0 @@
-require_relative '20130628025611_move_admin_notes_to_comments'
-require_relative '20130628025610_create_admin_notes'
-
-class DropActiveAdminComments < ActiveRecord::Migration
-  def change
-    revert MoveAdminNotesToComments
-    revert CreateAdminNotes
-  end
-end
