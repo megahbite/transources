ee96d8c6fdb2e2da7c0fd018cd57ba10c30cd16b
Megan Bowra-Dean <megan@rabidtech.co.nz>
2014-07-11 11:34:34 +1200

    Menu fixes

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -15,14 +15,6 @@ RSpec.configure do |config|
 
   config.infer_spec_type_from_file_location!
 
-  # ## Mock Framework
-  #
-  # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
-  #
-  # config.mock_with :mocha
-  # config.mock_with :flexmock
-  # config.mock_with :rr
-
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
