6a5715f77ce49fa8143fe720ce58c928fa7b3ce3
Megan Bowra-Dean <megan@rabidtech.co.nz>
2014-10-24 17:42:27 +1300

    Set asset host.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -42,6 +42,7 @@ Transources::Application.configure do
   # Use a different cache store in production
   # config.cache_store = :mem_cache_store
 
+  config.action_controller.asset_host = "https://transhealth.directory"
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
   # config.action_controller.asset_host = "http://assets.example.com"
 
